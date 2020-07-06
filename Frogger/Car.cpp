554039fa19cfc0d7584b64ddaeebb4e290b619df
Anna <annadanneker96@gmail.com>
2020-07-06 10:58:03 +0200

    mist

--- Frogger/Car.cpp
+++ Frogger/Car.cpp
@@ -13,16 +13,16 @@ void Car::draw(Terminal& term) {
 	//if (_dir == RIGHT) {
 	//for (int i = 0; i < car.size(); i++)
 	//{	//car right
-	term::set_cell(right_car[0].x, right_car[0].y, "- -");
-	term::set_cell(right_car[0].x, right_car[0].y, "0H>");
-	term::set_cell(right_car[0].x, right_car[0].y, "- -");
+/*	term.set_cell(right_car[0].x, right_car[0].y, "- -");
+	term.set_cell(right_car[0].x, right_car[0].y, "0H>");
+	term.set_cell(right_car[0].x, right_car[0].y, "- -");
 
 	//car left
 
-	term::set_cell(left_car[0].x, left_car[0].y, "- -");
-	term::set_cell(left_car[0].x, left_car[0].y, "<H0");
-	term::set_cell(left_car[0].x, left_car[0].y, "- -");
-	//}
+	term.set_cell(left_car[0].x, left_car[0].y, "- -");
+	term.set_cell(left_car[0].x, left_car[0].y, "<H0");
+	term.set_cell(left_car[0].x, left_car[0].y, "- -");
+	//}*/
 }
 /*
 void Car::move_car(std::vector<Vec2D>& car, Direction dir) {
