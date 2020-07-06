ade0e1b87d61846f2c8e319828f47d158d0747c1
Maiko Kuppe <maiko@instaffo.de>
2020-07-06 15:18:39 +0200

    läuft

--- Frogger/Car.cpp
+++ Frogger/Car.cpp
@@ -17,15 +17,15 @@ void Car::draw(Terminal& term) {
 	term.set_cell(_pos[0].x - 1, _pos[0].y, '-');
 	term.set_cell(_pos[0].x, _pos[0].y, ' ');
 	term.set_cell(_pos[0].x + 1, _pos[0].y, '-');
-	if (MoveDirection _dir == RIGHT) {
+	if (_dir == MoveDirection::RIGHT) {
 		term.set_cell(_pos[0].x - 1, _pos[0].y, '0');
 		term.set_cell(_pos[0].x, _pos[0].y, 'H');
 		term.set_cell(_pos[0].x + 1, _pos[0].y, '>');
 	}
-	if (MoveDirection _dir == LEFT) {
-		term.set_cell(left_car[0].x - 1, left_car[0].y, '<');
-		term.set_cell(left_car[0].x, left_car[0].y, 'H');
-		term.set_cell(left_car[0].x + 1, left_car[0].y, '0');
+	if (_dir == MoveDirection::LEFT) {
+		term.set_cell(_pos[0].x - 1, _pos[0].y, '<');
+		term.set_cell(_pos[0].x, _pos[0].y, 'H');
+		term.set_cell(_pos[0].x + 1, _pos[0].y, '0');
 	}
 	term.set_cell(_pos[0].x - 1, _pos[0].y, '-');
 	term.set_cell(_pos[0].x, _pos[0].y, ' ');
